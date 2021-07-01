412c2ee3c223940f3af93e93997d6cdd956495c0
mathru <user@mathru.net>
2021-07-01 09:26:05 +0900

    FormContextの機能追加

--- lib/form/form_context.dart
+++ lib/form/form_context.dart
@@ -80,4 +80,50 @@ class FormContext {
     key.currentState!.save();
     return true;
   }
+
+  /// Gets the value from the [key] of the map stored in [context].
+  ///
+  /// If [key] does not exist in the map, [orElse] is returned.
+  T get<T>(String key, T orElse) => _context.get(key, orElse);
+
+  /// Get the list corresponding to [key] in the map.
+  ///
+  /// If [key] is not found, the list of [orElse] is returned.
+  List<T> getAsList<T>(String key, [List<T>? orElse]) =>
+      _context.getAsList(key, orElse);
+
+  /// Get the map corresponding to [key] in the map.
+  ///
+  /// If [key] is not found, the map of [orElse] is returned.
+  Map<String, T> getAsMap<T>(String key, [Map<String, T>? orElse]) =>
+      _context.getAsMap(key, orElse);
+
+  /// Get the set corresponding to [key] in the map.
+  ///
+  /// If [key] is not found, the set of [orElse] is returned.
+  Set<T> getAsSets<T>(String key, [Set<T>? orElse]) =>
+      _context.getAsSets(key, orElse);
+
+  /// Get the set corresponding to [key] in the DateTime.
+  ///
+  /// If [key] is not found, the set of [orElse] is returned.
+  DateTime getAsDateTime(String key, [DateTime? orElse]) =>
+      _context.getAsDateTime(key, orElse);
+
+  /// Save [value] to [key] of the map stored in [context].
+  ///
+  /// The data will be unique within the page and can continue to be used even if the widget is rebuilt.
+  void operator []=(String key, dynamic value) {
+    _context[key] = value;
+  }
+
+  /// Copy the data stored in the form and [context] to [target].
+  ///
+  /// Only the data specified in the key of [keyAndInitialValues] will be copied.
+  ///
+  /// If there is no data for the specified key, the value of [keyAndInitialValues] is copied as the initial value.
+  T copyTo<T extends DynamicMap>(T target, DynamicMap keyAndInitialValues) {
+    target.copyFrom(_context, keyAndInitialValues);
+    return target;
+  }
 }
