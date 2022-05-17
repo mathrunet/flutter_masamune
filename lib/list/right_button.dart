a412447582d2db13af1e86ddc291e47c788c689c
mathru <user@mathru.net>
2022-05-17 17:02:45 +0900

    RightButtonのアップデート

--- lib/list/right_button.dart
+++ lib/list/right_button.dart
@@ -7,17 +7,27 @@ class RightButton extends StatelessWidget {
   /// Place button on the right.
   ///
   /// Please use it in the list.
-  const RightButton(
-      {this.color = Colors.blue, required this.label, required this.onPressed});
+  const RightButton({
+    this.color = Colors.blue,
+    required this.label,
+    required this.onPressed,
+    this.icon = Icons.open_in_new,
+    this.padding = const EdgeInsets.fromLTRB(0, 8, 16, 8),
+  });
 
   /// What happens when a button is pressed.
   final VoidCallback onPressed;
+
+  /// Icon.
+  final IconData icon;
 
   /// The collection path to read.
   final Color color;
 
   /// Button label.
   final String label;
+
+  final EdgeInsetsGeometry padding;
 
   /// Build method.
   ///
@@ -26,15 +36,19 @@ class RightButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.centerRight,
-      child: TextButton(
-        style: TextButton.styleFrom(
-          visualDensity: VisualDensity.compact,
-          padding: const EdgeInsets.all(0),
-        ),
-        onPressed: onPressed,
-        child: Text(
-          label,
-          style: TextStyle(color: color),
+      child: Padding(
+        padding: padding,
+        child: TextButton.icon(
+          icon: Icon(icon, color: color),
+          style: TextButton.styleFrom(
+            visualDensity: VisualDensity.compact,
+            padding: const EdgeInsets.all(0),
+          ),
+          onPressed: onPressed,
+          label: Text(
+            label,
+            style: TextStyle(color: color),
+          ),
         ),
       ),
     );
