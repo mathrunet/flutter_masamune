de6409070d7c616370aaa806a29e6567ef850a45
mathru <user@mathru.net>
2022-08-04 22:53:29 +0900

    fix: Get data for app command from spreadsheet

--- packages/masamune_cli/bin/command/app.dart
+++ packages/masamune_cli/bin/command/app.dart
@@ -14,14 +14,51 @@ class AppCliCommand extends CliCommand {
       print("App data could not be found.");
       return;
     }
+    var found = [];
+    final information = yaml["information"] as String?;
+    final account = app["account"] as String?;
     final id = app["bundle_id"] as String?;
+    if (id.isEmpty) {
+      print("bundle_id could not be found.");
+      return;
+    }
+    if (information != null && account.isNotEmpty) {
+      final endpoint = information
+          .replaceAllMapped(RegExp(r"/edit(#gid=([0-9]+))?$"), (match) {
+        final gid = match.group(2);
+        if (gid.isEmpty) {
+          return "/export?format=csv";
+        }
+        return "/export?format=csv&gid=$gid";
+      });
+      print("Load from $endpoint");
+      final request = await HttpClient().getUrl(Uri.parse(endpoint));
+      final response = await request.close();
+      final csv = await response.transform(utf8.decoder).join();
+      final raw = const CsvToListConverter().convert(csv);
+      for (int i = 1; i < raw.length; i++) {
+        final line = raw[i];
+        if (line.length <= 1) {
+          continue;
+        }
+        final id = line[1] as String;
+        if (id.isEmpty || id != account) {
+          continue;
+        }
+        found = line;
+      }
+    }
     final appId = (app["app_id"] as int?).toString();
-    final title = app["title"] as String?;
-    final titleShort = app["short_title"] as String?;
-    final description = app["description"] as String?;
-    final email = app["email"] as String?;
+    final title = app["title"] as String? ?? found[4];
+    final titleShort = app["short_title"] as String? ?? found[3];
+    if (title.isEmpty || titleShort.isEmpty) {
+      print("title could not be found.");
+      return;
+    }
+    final description = app["description"] as String? ?? found[5];
+    final email = app["email"] as String? ?? found[8];
     final color = app["color"] as String?;
-    final url = app["url"] as String?;
+    final url = app["url"] as String? ?? found[9];
     final image = app["image"] as String?;
     currentFiles.forEach((file) {
       var text = File(file.path).readAsStringSync();
