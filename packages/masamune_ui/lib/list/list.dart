9995e00b1c1bc01cfa80c3f1d34fafdbf3fb5817
mathru <45095843+mathrunet@users.noreply.github.com>
2022-09-05 22:22:52 +0900

    fix: Remove unused packages.

--- packages/masamune_ui/lib/list/list.dart
+++ packages/masamune_ui/lib/list/list.dart
@@ -13,7 +13,6 @@ import 'dart:math';
 import 'package:collapsible/collapsible.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
-import 'package:loading_animations/loading_animations.dart';
 import 'package:masamune_ui/masamune_ui.dart';
 
 export 'package:flutter_slidable/flutter_slidable.dart'
