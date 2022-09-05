81da5a21a63e652f6bd40d81df2e29ccf5674956
mathru <45095843+mathrunet@users.noreply.github.com>
2022-09-05 12:52:49 +0900

    fix(masamune_media): Fixed import bug.

--- packages/masamune_media/lib/dialog/mobile/mobile.dart
+++ packages/masamune_media/lib/dialog/mobile/mobile.dart
@@ -4,5 +4,6 @@ library masamune_media.dialog.mobile;
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:masamune/masamune.dart';
+import 'package:masamune_media/masamune_media.dart';
 
 part 'ui_media_dialog.dart';
