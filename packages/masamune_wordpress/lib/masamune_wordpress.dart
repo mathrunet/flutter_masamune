399cdafdb7d14f483532a17fe2f1d014ba033b53
mathru <45095843+mathrunet@users.noreply.github.com>
2022-09-05 12:12:04 +0900

    feat(masamune_wordpress)!: Initial commit.

---
+++ packages/masamune_wordpress/lib/masamune_wordpress.dart
@@ -0,0 +1,14 @@
+// Copyright 2022 mathru. All rights reserved.
+
+/// This package provide an API to connect Flutter and Wordpress.
+///
+/// To use, import `package:masamune/masamune.dart`.
+///
+/// [mathru.net]: https://mathru.net
+/// [YouTube]: https://www.youtube.com/c/mathrunetchannel
+library masamune_wordpress;
+
+// ignore: library_prefixes
+import 'package:wordpress_api/wordpress_api.dart' as WordPress;
+
+part 'src/wordpress_collection_model.dart';
