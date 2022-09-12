2a93023b676fe2f6edd9234f8bb8c6cebb0502ab
mathru <45095843+mathrunet@users.noreply.github.com>
2022-09-12 22:48:12 +0900

    feat(katana_annotation): Initial commitments.

---
+++ packages/katana_annotation/lib/katana_annotation.dart
@@ -0,0 +1,11 @@
+// Copyright 2022 mathru. All rights reserved.
+
+/// Definition of annotation when used in masamune.
+///
+/// To use, import `package:katana_annotation/katana_annotation.dart`.
+///
+/// [mathru.net]: https://mathru.net
+/// [YouTube]: https://www.youtube.com/c/mathrunetchannel
+library katana_annotation;
+
+part 'src/annotation.dart';
---
+++ packages/katana_annotation/lib/src/annotation.dart
@@ -0,0 +1,17 @@
+part of katana_annotation;
+
+/// Annotation indicating a model in Masamune.
+class ModelPath {
+  const ModelPath(this.path);
+
+  /// Paths for models.
+  final String path;
+}
+
+/// Annotation indicating a page in Masamune.
+class PagePath {
+  const PagePath(this.path);
+
+  /// Paths for models.
+  final String path;
+}
