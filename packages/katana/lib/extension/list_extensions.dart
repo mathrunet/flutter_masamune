7afc502dbbdf629b1d9e15d0207027b602c33e20
mathru <user@mathru.net>
2022-09-26 17:39:10 +0900

    fix(katana): Added <E> to get method.

--- packages/katana/lib/extension/list_extensions.dart
+++ packages/katana/lib/extension/list_extensions.dart
@@ -46,13 +46,13 @@ extension ListExtensions<T> on List<T> {
   /// Get the [index]th element.
   /// [index]番目の要素を取得します。
   ///
-  /// If [index] is out of range, [defaultValue] is returned.
-  /// [index]が範囲外の場合[defaultValue]が返されます。
-  T get(int index, T defaultValue) {
-    if (!containsIndex(index)) {
+  /// If [index] is out of range and the value is not [E], [defaultValue] is returned.
+  /// [index]が範囲外、値が[E]でない場合、[defaultValue]が返されます。
+  E get<E>(int index, E defaultValue) {
+    if (!containsIndex(index) || this[index] is! E) {
       return defaultValue;
     }
-    return this[index];
+    return this[index] as E;
   }
 
   /// Get a random element from [List].
--- packages/katana/lib/extension/nullable_list_extensions.dart
+++ packages/katana/lib/extension/nullable_list_extensions.dart
@@ -18,12 +18,12 @@ extension NullableListExtensions<T> on List<T>? {
   /// Get the [index]th element.
   /// [index]番目の要素を取得します。
   ///
-  /// If [index] is out of range or is itself [Null], [defaultValue] is returned.
-  /// [index]が範囲外、もしくは自身が[Null]の場合[defaultValue]が返されます。
-  T get(int index, T defaultValue) {
-    if (this == null || !containsIndex(index)) {
+  /// If [index] is out of range, or is itself [Null], or the value is not [E], [defaultValue] is returned.
+  /// [index]が範囲外、もしくは自身が[Null]、値が[E]でない場合、[defaultValue]が返されます。
+  E get<E>(int index, E defaultValue) {
+    if (this == null || !containsIndex(index) || this![index] is! E) {
       return defaultValue;
     }
-    return this![index];
+    return this![index] as E;
   }
 }
