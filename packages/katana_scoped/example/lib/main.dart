06a0780e95237adf47d560e8eed9d1a0b353bf0d
mathru <user@mathru.net>
2022-11-01 23:54:41 +0900

    docs(katana_scoped): EXAMPLE Maintenance.

--- packages/katana_scoped/example/lib/main.dart
+++ packages/katana_scoped/example/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return AppScoped(
       appRef: appRef,
       child: MaterialApp(
-        home: const ScopedTestPage(),
+        home: const CounterPage(),
         title: "Flutter Demo",
         theme: ThemeData(
           primarySwatch: Colors.blue,
@@ -26,16 +26,14 @@ class MyApp extends StatelessWidget {
 }
 
 class CounterPage extends PageScopedWidget {
-  const CounterPage({super.key, required this.title});
-
-  final String title;
+  const CounterPage({super.key});
 
   @override
   Widget build(BuildContext context, PageRef ref) {
-    final counter = ref.app.watch(() => ValueNotifier(0));
+    final counter = ref.page.watch(() => ValueNotifier(0));
 
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
+      appBar: AppBar(title: const Text("App Demo")),
       body: Center(
         child: Text("${counter.value}"),
       ),
@@ -46,100 +44,5 @@ class CounterPage extends PageScopedWidget {
         child: const Icon(Icons.add),
       ),
     );
-  }
-}
-
-class ScopedTestPage extends PageScopedWidget {
-  const ScopedTestPage({super.key});
-
-  @override
-  Widget build(BuildContext context, PageRef ref) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Test App"),
-      ),
-      body: ScopedTestContent(
-        page: this,
-      ),
-    );
-  }
-}
-
-class ScopedTestContent extends ScopedWidget<ScopedTestPage> {
-  const ScopedTestContent({
-    super.key,
-    required super.page,
-  });
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final widget = ref.widget.watch(() => ValueNotifier(0));
-    final page = ref.page.watch(() => ValueNotifier(0));
-    final app = ref.app.watch(() => ValueNotifier(0));
-
-    return Column(
-      children: [
-        ListTile(
-          title: Text(app.value.toString()),
-          onTap: () {
-            app.value++;
-          },
-        ),
-        ListTile(
-          title: Text(page.value.toString()),
-          onTap: () {
-            page.value++;
-          },
-        ),
-        ListTile(
-          title: Text(widget.value.toString()),
-          onTap: () {
-            widget.value++;
-          },
-        ),
-      ],
-    );
-  }
-}
-
-class Repository with ChangeNotifier {}
-
-class RepositoryValue extends ScopedValue<Repository> {
-  @override
-  ScopedValueState<Repository, RepositoryValue> createState() =>
-      RepositoryValueState();
-}
-
-class RepositoryValueState
-    extends ScopedValueState<Repository, RepositoryValue> {
-  late Repository repository;
-
-  @override
-  void initValue() {
-    super.initValue();
-    repository = Repository();
-    repository.addListener(_handeldOnUpdate);
-  }
-
-  void _handeldOnUpdate() {
-    setState(() {});
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    repository.removeListener(_handeldOnUpdate);
-    repository.dispose();
-  }
-
-  @override
-  Repository build() => repository;
-}
-
-extension UserRepositoryAppRef on RefHasApp {
-  TRepository repository<TRepository extends Repository>(
-    TRepository source,
-  ) {
-    return app.watch(() => source);
   }
 }
