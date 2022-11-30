63c95575b6c37a4bd8d4b9ca6825b1f69abdc86c
mathru <user@mathru.net>
2022-11-30 23:04:16 +0900

    feat: Initial commit.

---
+++ packages/katana_media/lib/katana_media.dart
@@ -0,0 +1,11 @@
+// Copyright 2023 mathru. All rights reserved.
+
+/// Base package for retrieving files such as images and videos from terminal storage, cameras, etc.
+///
+/// To use, import `package:katana_media/katana_media.dart`.
+///
+/// [mathru.net]: https://mathru.net
+/// [YouTube]: https://www.youtube.com/c/mathrunetchannel
+library katana_media;
+
+import 'dart:async';
---
+++ packages/katana_media_mobile/lib/katana_media_mobile.dart
@@ -0,0 +1,11 @@
+// Copyright 2023 mathru. All rights reserved.
+
+/// Mobile plugin for katana_media. Base package for retrieving files such as images and videos from terminal storage, cameras, etc.
+///
+/// To use, import `package:katana_media_mobile/katana_media_mobile.dart`.
+///
+/// [mathru.net]: https://mathru.net
+/// [YouTube]: https://www.youtube.com/c/mathrunetchannel
+library katana_media_mobile;
+
+import 'dart:async';
