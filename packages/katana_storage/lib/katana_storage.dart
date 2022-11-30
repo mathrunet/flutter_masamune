80a32e8d97319e9e99cbb766838a5229e97324d8
mathru <user@mathru.net>
2022-11-30 23:04:36 +0900

    feat: Initial commit.

---
+++ packages/katana_storage/lib/katana_storage.dart
@@ -0,0 +1,11 @@
+// Copyright 2023 mathru. All rights reserved.
+
+/// Base package to facilitate switching between Local and Firebase storage implementations.
+///
+/// To use, import `package:katana_storage/katana_storage.dart`.
+///
+/// [mathru.net]: https://mathru.net
+/// [YouTube]: https://www.youtube.com/c/mathrunetchannel
+library katana_storage;
+
+import 'dart:async';
---
+++ packages/katana_storage_firebase/lib/katana_storage_firebase.dart
@@ -0,0 +1,11 @@
+// Copyright 2023 mathru. All rights reserved.
+
+/// Firebase plugin for katana_storage. A package to facilitate switching between Local and Firebase storage implementations.
+///
+/// To use, import `package:katana_storage_firebase/katana_storage_firebase.dart`.
+///
+/// [mathru.net]: https://mathru.net
+/// [YouTube]: https://www.youtube.com/c/mathrunetchannel
+library katana_storage_firebase;
+
+import 'dart:async';
