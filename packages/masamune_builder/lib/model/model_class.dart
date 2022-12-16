3fdc19a979ee524eb29d7f77f0ead64e601a8912
mathru <user@mathru.net>
2022-12-17 00:32:57 +0900

    fix: Added ModelRef addition mechanism.

--- packages/masamune_builder/lib/model/model_class.dart
+++ packages/masamune_builder/lib/model/model_class.dart
@@ -1,6 +1,6 @@
 part of masamune_builder;
 
-final _regExpModelRef = RegExp(r"ModelRef<([^>]+)>");
+final _regExpModelRef = RegExp(r"ModelRef(Base)?<([^>]+)>");
 final _regExpRef = RegExp(r"(.+)Ref");
 
 List<Spec> modelClass(
@@ -94,32 +94,33 @@ List<Spec> modelClass(
                 ..name = "builder"
                 ..lambda = true
                 ..type = MethodType.getter
-                ..returns = Reference("List<ModelRefBuilder<${model.name}>>")
+                ..returns =
+                    Reference("List<ModelRefBuilderBase<${model.name}>>")
                 ..annotations.addAll([const Reference("override")])
                 ..body = Code("[${referencable.map((e) {
                   if (e.type.toString().endsWith("Ref")) {
                     final match = _regExpRef.firstMatch(e.type.toString());
                     if (match == null) {
                       throw Exception(
-                        "@refParam can only be given to ModelRef<T> types. \r\n\r\n${e.type} ${e.name}",
+                        "@refParam can only be given to ModelRef<T> / ModelRefBase<T>? / XXXRef types. \r\n\r\n${e.type} ${e.name}",
                       );
                     }
                     final doc = "\$${match.group(1)}Document";
-                    return "ModelRefBuilder( modelRef: (value) => value.${e.name}, document: (modelQuery) => $doc(modelQuery), value: (value, doc) => value.copyWith( ${e.name}: doc as ${match.group(1)}Ref ),)";
+                    return "ModelRefBuilder( modelRef: (value) => value.${e.name}, document: (modelQuery) => $doc(modelQuery), value: (value, doc) => value.copyWith( ${e.name}: doc ),)";
                   } else {
                     if (!e.type.toString().endsWith("?")) {
                       throw Exception(
-                        "ModelRef<T> must be nullable. \r\n\r\n${e.type} ${e.name}",
+                        "ModelRefBase<T> must be nullable. \r\n\r\n${e.type} ${e.name}",
                       );
                     }
                     final match = _regExpModelRef.firstMatch(e.type.toString());
                     if (match == null) {
                       throw Exception(
-                        "@refParam can only be given to ModelRef<T> types. \r\n\r\n${e.type} ${e.name}",
+                        "@refParam can only be given to ModelRef<T> / ModelRefBase<T>? / XXXRef types. \r\n\r\n${e.type} ${e.name}",
                       );
                     }
-                    final doc = "\$${match.group(1)}Document";
-                    return "ModelRefBuilder( modelRef: (value) => value.${e.name}, document: (modelQuery) => $doc(modelQuery), value: (value, doc) => value.copyWith( ${e.name}: doc as ${match.group(1)}Ref ),)";
+                    final doc = "\$${match.group(2)}Document";
+                    return "ModelRefBuilder( modelRef: (value) => value.${e.name}, document: (modelQuery) => $doc(modelQuery), value: (value, doc) => value.copyWith( ${e.name}: doc ),)";
                   }
                 }).join(",")}]"),
             ),
