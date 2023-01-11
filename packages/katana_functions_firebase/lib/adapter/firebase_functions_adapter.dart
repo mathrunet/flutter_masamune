a0b2317c09b39750e34bab8522a8e75645e0fe98
mathru <user@mathru.net>
2023-01-11 23:58:06 +0900

    feat: Initial commit.

---
+++ packages/katana_functions_firebase/lib/adapter/firebase_functions_adapter.dart
@@ -0,0 +1,53 @@
+part of katana_functions_firebase;
+
+/// Adapter to return server-side processing using Firebase Functions.
+///
+/// The server-side implementation can be simplified by using `@mathrunet/mathamune` in npm.
+///
+/// Firebase Functionsを利用してサーバー側の処理を返すためのアダプター。
+///
+/// npmの`@mathrunet/masamune`を利用することでサーバー側の実装を簡略化することができます。
+class FirebaseFunctionsAdapter extends FunctionsAdapter {
+  /// Adapter to return server-side processing using Firebase Functions.
+  ///
+  /// The server-side implementation can be simplified by using `@mathrunet/mathamune` in npm.
+  ///
+  /// Firebase Functionsを利用してサーバー側の処理を返すためのアダプター。
+  ///
+  /// npmの`@mathrunet/masamune`を利用することでサーバー側の実装を簡略化することができます。
+  const FirebaseFunctionsAdapter();
+
+  /// Instances of Firebase Functions.
+  ///
+  /// Firebase Functionsのインスタンス。
+  FirebaseFunctions get functions {
+    return FirebaseFunctions.instance;
+  }
+
+  @override
+  Future<void> sendNotification({
+    required String title,
+    required String text,
+    String? channel,
+    DynamicMap? data,
+    required String target,
+  }) async {
+    try {
+      final regExp = RegExp(r"[a-zA-Z0-9]{11}:[0-9a-zA-Z_-]+");
+      final isToken = regExp.hasMatch(target);
+      await FirebaseCore.initialize();
+      await functions.httpsCallable("send_notification").call(
+        {
+          "title": title,
+          "text": text,
+          if (channel != null) "channel_id": channel,
+          if (data != null) "data": data,
+          if (isToken) "token": target else "topic": target,
+        },
+      );
+    } catch (e) {
+      print(e.toString());
+      rethrow;
+    }
+  }
+}
---
+++ packages/katana_functions_firebase/lib/katana_functions_firebase.dart
@@ -0,0 +1,18 @@
+// Copyright 2023 mathru. All rights reserved.
+
+/// Provides an adapter for use with Firebase Functions. Provides an interface to execute server-side processing in a type-safe manner. Actual processing on the server side is done by importing a separate adapter.
+///
+/// To use, import `package:katana_functions_firebase/katana_functions_firebase.dart`.
+///
+/// [mathru.net]: https://mathru.net
+/// [YouTube]: https://www.youtube.com/c/mathrunetchannel
+library katana_functions_firebase;
+
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:flutter/widgets.dart';
+import 'package:katana_firebase/katana_firebase.dart';
+import 'package:katana_functions/katana_functions.dart';
+
+export 'package:katana_functions/katana_functions.dart';
+
+part 'adapter/firebase_functions_adapter.dart';
