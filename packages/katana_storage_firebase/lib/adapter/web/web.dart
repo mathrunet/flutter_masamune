0cb557fda2580cbf666279516ffdb380ff7f50de
mathru <user@mathru.net>
2023-01-19 18:27:58 +0900

    fix: Remove unnecessary imports.

--- packages/katana_storage_firebase/lib/adapter/web/web.dart
+++ packages/katana_storage_firebase/lib/adapter/web/web.dart
@@ -1,11 +1,8 @@
 /// For web.
 library katana_storage_firebase.web;
 
-import 'dart:typed_data';
-
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
-import 'package:katana_firebase/katana_firebase.dart';
 import 'package:katana_storage/katana_storage.dart';
 
 part 'firebase_storage_adapter.dart';
