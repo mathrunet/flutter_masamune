8bc3fc8d96e33a428155fcebfaeeb2579edea8ac
mathru <user@mathru.net>
2023-02-03 15:42:05 +0900

    chore: Fixes in the build_runner of Sample.

--- packages/katana_router/example/lib/main.page.dart
+++ packages/katana_router/example/lib/main.page.dart
@@ -1,3 +1,5 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
 // ignore_for_file: unused_field, unused_element, require_trailing_commas, prefer_const_constructors, unnecessary_overrides, prefer_const_literals_to_create_immutables,  unnecessary_null_in_if_null_operators, library_prefixes, directives_ordering, unused_local_variable
 
 part of 'main.dart';
--- packages/katana_router/example/lib/main.router.dart
+++ packages/katana_router/example/lib/main.router.dart
@@ -1,3 +1,5 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
 // ignore_for_file: unused_field, unused_element, require_trailing_commas, prefer_const_constructors, unnecessary_overrides, prefer_const_literals_to_create_immutables,  unnecessary_null_in_if_null_operators, library_prefixes, directives_ordering, depend_on_referenced_packages
 
 // **************************************************************************
@@ -5,16 +7,9 @@
 // **************************************************************************
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-
-// Flutter imports:
 import 'package:flutter/material.dart';
-
-// Package imports:
 import 'package:katana_router/katana_router.dart';
-
-// Project imports:
 import 'package:katana_router_example/main.dart' as _$1;
-
 export 'package:katana_router_example/main.dart'
     show MainPage, UserPage, ContentPage, NestedContainerPage;
 
