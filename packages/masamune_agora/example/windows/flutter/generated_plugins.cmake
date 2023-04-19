41bac310c7bf3e600c3edb80a9e143b1223cba9c
mathru <user@mathru.net>
2023-04-19 16:09:32 +0900

    fix: Fix "Don't use web-only libraries outside Flutter web plugin packages.

--- packages/masamune_agora/example/windows/flutter/generated_plugins.cmake
+++ packages/masamune_agora/example/windows/flutter/generated_plugins.cmake
@@ -4,6 +4,7 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   agora_rtc_engine
+  permission_handler_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
--- packages/masamune_agora/lib/src/web/web.dart
+++ packages/masamune_agora/lib/src/web/web.dart
@@ -1,7 +1,7 @@
 // For web.
-library masamune_agora.web;
+// ignore_for_file: avoid_web_libraries_in_flutter
 
-// ignore: avoid_web_libraries_in_flutter
+library masamune_agora.web;
 
 // Dart imports:
 import 'dart:html';
