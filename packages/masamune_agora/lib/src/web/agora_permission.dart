84c6ff2b7dae7a52dfc42bf1a9a53f8a2aeee118
mathru <user@mathru.net>
2023-04-19 13:38:33 +0900

    docs: Updated comments.

--- packages/masamune_agora/lib/src/web/agora_permission.dart
+++ packages/masamune_agora/lib/src/web/agora_permission.dart
@@ -1,8 +1,29 @@
 part of masamune_agora.web;
 
+/// A class that manages the authorization area.
+///
+/// Requests the use of privileges with [request].
+///
+/// 権限周りを管理するクラス。
+///
+/// [request]で権限の利用を要求します。
 class AgoraPermission {
+  /// A class that manages the authorization area.
+  ///
+  /// Requests the use of privileges with [request].
+  ///
+  /// 権限周りを管理するクラス。
+  ///
+  /// [request]で権限の利用を要求します。
   const AgoraPermission();
 
+  /// Requests the use of privileges.
+  ///
+  /// If [enableVideo] and [enableAudio] are set to `true` respectively, the related permissions are checked & requested.
+  ///
+  /// 権限の利用を要求します。
+  ///
+  /// [enableVideo]と[enableAudio]をそれぞれ`true`にすると関連する権限をチェック＆リクエストします。
   Future<bool> request({
     bool enableVideo = true,
     bool enableAudio = true,
