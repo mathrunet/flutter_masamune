27c51d1509d4b24d31ad9379ef823ab3c0f564f3
mathru <user@mathru.net>
2023-05-31 00:13:16 +0900

    fix: Added ability to update own tabs in openURL.

--- packages/masamune_universal_ui/lib/src/functions.dart
+++ packages/masamune_universal_ui/lib/src/functions.dart
@@ -2,10 +2,20 @@ part of masamune_universal_ui;
 
 /// Open a new external [url].
 ///
+/// In case of Web, setting [openSelfWindowOnWeb] to `true` will open the window in its own window (tab).
+///
 /// 新しい外部[url]を開きます。
-Future<void> openURL(String url) async {
+///
+/// Webの場合[openSelfWindowOnWeb]を`true`にすると自身のウインドウ（タブ）で開きます。
+Future<void> openURL(
+  String url, {
+  bool openSelfWindowOnWeb = false,
+}) async {
   if (await canLaunchUrlString(url)) {
-    await launchUrlString(url);
+    await launchUrlString(
+      url,
+      webOnlyWindowName: kIsWeb && openSelfWindowOnWeb ? "_self" : null,
+    );
   } else {
     throw Exception("Could not Launch $url");
   }
