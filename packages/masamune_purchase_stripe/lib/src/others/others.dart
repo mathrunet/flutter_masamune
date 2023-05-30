7fb85a6d9e23875a82d17f18ae5691ebaa9f786d
mathru <user@mathru.net>
2023-05-30 19:07:16 +0900

    chore: Import sorter.

--- packages/masamune_purchase_stripe/lib/src/others/others.dart
+++ packages/masamune_purchase_stripe/lib/src/others/others.dart
@@ -1,7 +1,10 @@
 // For others.
 library masamune_purchase_stripe.others;
 
+// Flutter imports:
 import 'package:flutter/material.dart';
+
+// Package imports:
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:universal_platform/universal_platform.dart';
 
--- packages/masamune_purchase_stripe/lib/src/web/web.dart
+++ packages/masamune_purchase_stripe/lib/src/web/web.dart
@@ -1,6 +1,7 @@
 // For web.
 library masamune_purchase_stripe.web;
 
+// Flutter imports:
 import 'package:flutter/material.dart';
 
 part 'stripe_webview.dart';
