70dfb6e52a28d650ea61588cdfe1939e089802fa
mathru <user@mathru.net>
2023-07-13 20:05:46 +0900

    fix: Apply import sorter.

--- packages/masamune_purchase_mobile/lib/adapter/others/others.dart
+++ packages/masamune_purchase_mobile/lib/adapter/others/others.dart
@@ -1,16 +1,22 @@
 // For others.
 library masamune_purchase_mobile.others;
 
+// Dart imports:
 import 'dart:async';
 
+// Flutter imports:
 import 'package:flutter/widgets.dart';
+
+// Package imports:
 import 'package:in_app_purchase/in_app_purchase.dart';
 import 'package:in_app_purchase_android/in_app_purchase_android.dart';
 import 'package:in_app_purchase_storekit/in_app_purchase_storekit.dart';
 import 'package:in_app_purchase_storekit/store_kit_wrappers.dart';
 import 'package:masamune/masamune.dart';
-import 'package:masamune_purchase_mobile/masamune_purchase_mobile.dart';
 import 'package:universal_platform/universal_platform.dart';
+
+// Project imports:
+import 'package:masamune_purchase_mobile/masamune_purchase_mobile.dart';
 
 part 'purchase_product.dart';
 part 'mobile_purchase_masamune_adapter.dart';
--- packages/masamune_purchase_mobile/lib/adapter/web/web.dart
+++ packages/masamune_purchase_mobile/lib/adapter/web/web.dart
@@ -2,10 +2,16 @@
 
 library masamune_purchase_mobile.web;
 
+// Dart imports:
 import 'dart:async';
 
+// Flutter imports:
 import 'package:flutter/widgets.dart';
+
+// Package imports:
 import 'package:masamune/masamune.dart';
+
+// Project imports:
 import 'package:masamune_purchase_mobile/masamune_purchase_mobile.dart';
 
 part 'mobile_purchase_masamune_adapter.dart';
