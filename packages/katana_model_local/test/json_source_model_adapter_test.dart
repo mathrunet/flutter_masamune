0737a6b2735641b36f97d1e5dc774cea001bf0e4
mathru <user@mathru.net>
2023-08-19 17:18:38 +0900

    chore: Import sorter.

--- packages/katana_model_local/test/json_source_model_adapter_test.dart
+++ packages/katana_model_local/test/json_source_model_adapter_test.dart
@@ -1,9 +1,7 @@
 // ignore_for_file: avoid_print
 
-// Flutter imports:
+// Dart imports:
 import 'dart:convert';
-
-import 'package:flutter/painting.dart';
 
 // Package imports:
 import 'package:freezed_annotation/freezed_annotation.dart';
