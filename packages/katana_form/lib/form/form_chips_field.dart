6712c902c6526239ee6a8e187cb2693b73e6b0f6
mathru <user@mathru.net>
2023-10-24 13:53:42 +0900

    chore: Apply input formatter.

--- packages/katana_form/lib/form/form_chips_field.dart
+++ packages/katana_form/lib/form/form_chips_field.dart
@@ -389,51 +389,52 @@ class _FormChipsField<TValue> extends FormFieldState<List<String>>
               child: _ChipsInput<String>(
                 initialValue: value ?? [],
                 decoration: InputDecoration(
-                  contentPadding: widget.style?.contentPadding ??
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 0),
-                  fillColor: widget.style?.backgroundColor,
-                  filled: widget.style?.backgroundColor != null,
-                  isDense: true,
-                  border: widget.style?.border ?? borderSide,
-                  enabledBorder: widget.style?.border ?? borderSide,
-                  disabledBorder: widget.style?.disabledBorder ??
-                      widget.style?.border ??
-                      borderSide,
-                  errorBorder: widget.style?.errorBorder ??
-                      widget.style?.border ??
-                      borderSide,
-                  focusedBorder: widget.style?.border ?? borderSide,
-                  focusedErrorBorder: widget.style?.errorBorder ??
-                      widget.style?.border ??
-                      borderSide,
-                  hintText: widget.hintText,
-                  labelText: widget.labelText,
-                  prefix: widget.prefix?.child ?? widget.style?.prefix?.child,
-                  suffix: widget.suffix?.child ?? widget.style?.suffix?.child,
-                  prefixIcon: widget.prefix?.icon ?? widget.style?.prefix?.icon,
-                  suffixIcon: widget.suffix?.icon ?? widget.style?.suffix?.icon,
-                  prefixText:
-                      widget.prefix?.label ?? widget.style?.prefix?.label,
-                  suffixText:
-                      widget.suffix?.label ?? widget.style?.suffix?.label,
-                  prefixIconColor: widget.prefix?.iconColor ??
-                      widget.style?.prefix?.iconColor,
-                  suffixIconColor: widget.suffix?.iconColor ??
-                      widget.style?.suffix?.iconColor,
-                  prefixIconConstraints: widget.prefix?.iconConstraints ??
-                      widget.style?.prefix?.iconConstraints,
-                  suffixIconConstraints: widget.suffix?.iconConstraints ??
-                      widget.style?.suffix?.iconConstraints,
-                  labelStyle:
-                      widget.enabled ? mainTextStyle : disabledTextStyle,
-                  hintStyle: subTextStyle,
-                  suffixStyle: subTextStyle,
-                  prefixStyle: subTextStyle,
-                  counterStyle: subTextStyle,
-                  helperStyle: subTextStyle,
-                  errorStyle: errorTextStyle,
-                  errorText: errorText
-                ),
+                    contentPadding: widget.style?.contentPadding ??
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 0),
+                    fillColor: widget.style?.backgroundColor,
+                    filled: widget.style?.backgroundColor != null,
+                    isDense: true,
+                    border: widget.style?.border ?? borderSide,
+                    enabledBorder: widget.style?.border ?? borderSide,
+                    disabledBorder: widget.style?.disabledBorder ??
+                        widget.style?.border ??
+                        borderSide,
+                    errorBorder: widget.style?.errorBorder ??
+                        widget.style?.border ??
+                        borderSide,
+                    focusedBorder: widget.style?.border ?? borderSide,
+                    focusedErrorBorder: widget.style?.errorBorder ??
+                        widget.style?.border ??
+                        borderSide,
+                    hintText: widget.hintText,
+                    labelText: widget.labelText,
+                    prefix: widget.prefix?.child ?? widget.style?.prefix?.child,
+                    suffix: widget.suffix?.child ?? widget.style?.suffix?.child,
+                    prefixIcon:
+                        widget.prefix?.icon ?? widget.style?.prefix?.icon,
+                    suffixIcon:
+                        widget.suffix?.icon ?? widget.style?.suffix?.icon,
+                    prefixText:
+                        widget.prefix?.label ?? widget.style?.prefix?.label,
+                    suffixText:
+                        widget.suffix?.label ?? widget.style?.suffix?.label,
+                    prefixIconColor: widget.prefix?.iconColor ??
+                        widget.style?.prefix?.iconColor,
+                    suffixIconColor: widget.suffix?.iconColor ??
+                        widget.style?.suffix?.iconColor,
+                    prefixIconConstraints: widget.prefix?.iconConstraints ??
+                        widget.style?.prefix?.iconConstraints,
+                    suffixIconConstraints: widget.suffix?.iconConstraints ??
+                        widget.style?.suffix?.iconConstraints,
+                    labelStyle:
+                        widget.enabled ? mainTextStyle : disabledTextStyle,
+                    hintStyle: subTextStyle,
+                    suffixStyle: subTextStyle,
+                    prefixStyle: subTextStyle,
+                    counterStyle: subTextStyle,
+                    helperStyle: subTextStyle,
+                    errorStyle: errorTextStyle,
+                    errorText: errorText),
                 obscureText: widget.obscureText,
                 enabled: widget.enabled,
                 inputType: widget.keyboardType,
