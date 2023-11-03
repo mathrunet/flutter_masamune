2a00b84dabe343db97a3943bb767629aed3743d1
mathru <user@mathru.net>
2023-11-03 09:49:18 +0900

    feat: Initial commit.

---
+++ packages/masamune_model_planetscale_annotation/lib/src/planet_scale_auto_increment_filed.dart
@@ -0,0 +1,28 @@
+part of masamune_model_planetscale_annotation;
+
+/// When using [PlanetScale], specify for fields that are auto-incremental.
+///
+/// Can only be specified for [int].
+///
+/// [PlanetScale]を利用する場合、オートインクリメントになるフィールドに対して指定します。
+///
+/// [int]に対してのみ指定できます。
+const planetScaleAutoIncrementField = PlanetScaleAutoIncrementField();
+
+/// When using [PlanetScale], specify for fields that are auto-incremental.
+///
+/// Can only be specified for [int].
+///
+/// [PlanetScale]を利用する場合、オートインクリメントになるフィールドに対して指定します。
+///
+/// [int]に対してのみ指定できます。
+class PlanetScaleAutoIncrementField {
+  /// When using [PlanetScale], specify for fields that are auto-incremental.
+  ///
+  /// Can only be specified for [int].
+  ///
+  /// [PlanetScale]を利用する場合、オートインクリメントになるフィールドに対して指定します。
+  ///
+  /// [int]に対してのみ指定できます。
+  const PlanetScaleAutoIncrementField();
+}
---
+++ packages/masamune_model_planetscale_annotation/lib/src/planet_scale_unique_filed.dart
@@ -0,0 +1,28 @@
+part of masamune_model_planetscale_annotation;
+
+/// When using [PlanetScale], specify for fields whose values should not be covered.
+///
+/// Returns an error if a value is stored that is covered.
+///
+/// [PlanetScale]を利用する場合、値が被らないようにするフィールドに対して指定します。
+///
+/// 被る値が保存された場合エラーを返します。
+const planetScaleUniqueField = PlanetScaleUniqueField();
+
+/// When using [PlanetScale], specify for fields whose values should not be covered.
+///
+/// Returns an error if a value is stored that is covered.
+///
+/// [PlanetScale]を利用する場合、値が被らないようにするフィールドに対して指定します。
+///
+/// 被る値が保存された場合エラーを返します。
+class PlanetScaleUniqueField {
+  /// When using [PlanetScale], specify for fields whose values should not be covered.
+  ///
+  /// Returns an error if a value is stored that is covered.
+  ///
+  /// [PlanetScale]を利用する場合、値が被らないようにするフィールドに対して指定します。
+  ///
+  /// 被る値が保存された場合エラーを返します。
+  const PlanetScaleUniqueField();
+}
