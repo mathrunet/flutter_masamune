c0b2c0e945d9dd869c2fb190c2e08a23d0311c9e
mathru <user@mathru.net>
2023-12-11 21:11:21 +0900

    chore: Package Update.

--- packages/masamune_auth_google/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/masamune_auth_google/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import google_sign_in_ios
 import path_provider_foundation
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
