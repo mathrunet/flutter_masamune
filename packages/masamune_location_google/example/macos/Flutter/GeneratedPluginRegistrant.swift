95b2f1720c539ff92a82d88d95e3230ced08bb22
mathru <user@mathru.net>
2024-01-17 00:01:15 +0900

    fix: Change the Example package.

--- packages/masamune_location_google/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/masamune_location_google/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import geolocator_apple
+import location
 import path_provider_foundation
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
