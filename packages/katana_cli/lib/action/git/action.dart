5b43ee0e4d5fa76ec659d4a967da6eb9e37aa7d3
mathru <user@mathru.net>
2024-01-17 17:19:49 +0900

    chore: Change error statement

--- packages/katana_cli/lib/action/git/action.dart
+++ packages/katana_cli/lib/action/git/action.dart
@@ -40,7 +40,7 @@ class GitActionCliAction extends CliCommand with CliActionMixin {
     final platforms = action.get("platform", "").trimString(" ").split(" ");
     if (platforms.isEmpty) {
       error(
-        "Platform is not specified. Please pass the platform you want to support as a parameter, like `katana github action android ios web`. Supported platforms are `android`, `ios`, `web`, `windows`, `macos`, and `linux`.",
+        "Platform is not specified. Please pass the platform you want to support as a parameter. Supported platforms are `android`, `ios`, `web`, `windows`, `macos`, and `linux`.",
       );
       return;
     }
