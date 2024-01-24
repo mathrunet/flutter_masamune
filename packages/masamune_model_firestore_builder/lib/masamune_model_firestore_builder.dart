6e93824a525d9f7a5a9e7b869605b79828453f0a
mathru <user@mathru.net>
2024-01-24 17:22:49 +0900

    chore: Apply Import sorter.

--- packages/masamune_model_firestore_builder/lib/masamune_model_firestore_builder.dart
+++ packages/masamune_model_firestore_builder/lib/masamune_model_firestore_builder.dart
@@ -8,8 +8,10 @@
 /// [YouTube]: https://www.youtube.com/c/mathrunetchannel
 library masamune_model_firestore_builder;
 
+// Dart imports:
 import 'dart:io';
 
+// Package imports:
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/element/type.dart';
 import 'package:build/build.dart';
