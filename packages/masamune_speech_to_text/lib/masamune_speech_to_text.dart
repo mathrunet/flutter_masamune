77633077f043a6c1ac6e37a33092191ea205c935
mathru <user@mathru.net>
2024-01-28 23:03:09 +0900

    fix: Remove it because the class was covered.

--- packages/masamune_speech_to_text/lib/masamune_speech_to_text.dart
+++ packages/masamune_speech_to_text/lib/masamune_speech_to_text.dart
@@ -25,7 +25,8 @@ import 'package:masamune/masamune.dart';
 import 'package:speech_to_text/speech_recognition_error.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart';
-import 'package:speech_to_text_platform_interface/speech_to_text_platform_interface.dart';
+import 'package:speech_to_text_platform_interface/speech_to_text_platform_interface.dart'
+    hide ListenMode;
 
 part 'adapter/speech_to_text_masamune_adapter.dart';
 part 'src/speech_to_text_controller.dart';
