a702db62ee3c5e971ea51575f006e5c6d7a3507c
mathru <user@mathru.net>
2024-04-19 16:33:00 +0900

    chore: Import sorter.

--- packages/katana_scoped/test/refresh_test.dart
+++ packages/katana_scoped/test/refresh_test.dart
@@ -12,9 +12,9 @@ import 'package:flutter/material.dart';
 
 // Package imports:
 import 'package:flutter_test/flutter_test.dart';
-import 'package:katana_scoped/katana_scoped.dart';
 
 // Project imports:
+import 'package:katana_scoped/katana_scoped.dart';
 import 'test_extensions.dart';
 
 void main() {
