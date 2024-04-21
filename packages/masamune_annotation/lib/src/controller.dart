2768a77ff6d691a15c7efd41d5869c8845f62b37
mathru <user@mathru.net>
2024-04-21 09:06:03 +0900

    fix: The controller's annotations are now disabled directly.

--- packages/masamune_annotation/lib/src/controller.dart
+++ packages/masamune_annotation/lib/src/controller.dart
@@ -25,33 +25,6 @@ part of '/masamune_annotation.dart';
 ///   static const query = _$UserControllerQuery();
 /// }
 /// ```
-const controller = Controller();
-
-/// Annotation for creating queries for controllers.
-///
-/// You can define a query to retrieve the controller at `static const query = _$(class name)ControllerQuery()`.
-///
-/// Used for classes inheriting from `ChangeNotifier`.
-///
-/// If [autoDisposeWhenUnreferenced] is set to `true`, it will automatically dispose of [Controller] when it is no longer referenced by any widget.
-///
-/// コントローラー用のクエリを作成するためのアノテーション。
-///
-/// `static const query = _$(クラス名)ControllerQuery()`にコントローラーを取得するためのクエリを定義できます。
-///
-/// `ChangeNotifier`を継承したクラスに対して利用します。
-///
-/// [autoDisposeWhenUnreferenced]を`true`にした場合、[Controller]がどのウィジェットにも参照されなくなったときに自動的に破棄します。
-///
-/// ```dart
-/// @controller
-/// @listenables
-/// class UserController extends ChangeNotifier {
-///   UserController();
-///
-///   static const query = _$UserControllerQuery();
-/// }
-/// ```
 class Controller {
   /// Annotation for creating queries for controllers.
   ///
--- packages/masamune_annotation/lib/src/controller_group.dart
+++ packages/masamune_annotation/lib/src/controller_group.dart
@@ -31,39 +31,6 @@ part of '/masamune_annotation.dart';
 /// ```
 ///
 /// * see https://pub.dev/packages/katana_listenables
-const controllerGroup = ControllerGroup();
-
-/// Annotation to create a query for a group of controllers.
-///
-/// You can define a query to retrieve the controller at `static const query = _$(class name)ControllerGroupQuery()`.
-///
-/// Multiple `ChangeNotifier`s can be grouped together in `katana_listenables` and used as a controller group.
-///
-/// If [autoDisposeWhenUnreferenced] is set to `true`, [ControllerGroup] will be automatically disposed of when it is no longer referenced by any widget.
-///
-/// コントローラーグループ用のクエリを作成するためのアノテーション。
-///
-/// `static const query = _$(クラス名)ControllerGroupQuery()`にコントローラーを取得するためのクエリを定義できます。
-///
-/// `katana_listenables`で複数の`ChangeNotifier`をまとめた上でコントローラーグループとして利用することが可能です。
-///
-/// [autoDisposeWhenUnreferenced]を`true`にした場合、[ControllerGroup]がどのウィジェットにも参照されなくなったときに自動的に破棄します。
-///
-/// ```dart
-/// @listenables
-/// @controllerGroup
-/// class PageControllerGroup with _$PageControllerGroup, ChangeNotifier {
-///   factory ${className}ControllerGroup({
-///     TextEditingController? textEditingController,
-///     FocusNode? focusNode,
-///   }) = _PageControllerGroup;
-///   PageControllerGroup._();
-///
-///   static const query = _$PageControllerGroupQuery();
-/// }
-/// ```
-///
-/// * see https://pub.dev/packages/katana_listenables
 class ControllerGroup {
   /// Annotation to create a query for a group of controllers.
   ///
