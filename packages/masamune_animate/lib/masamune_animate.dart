54c355cd53971c768aa32a87162043edc4af908d
mathru <user@mathru.net>
2024-04-25 01:45:19 +0900

    feat: Initial commit.

---
+++ packages/masamune_animate/lib/masamune_animate.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2024 mathru. All rights reserved.
+
+/// A package for animation that runs on the Masamune framework.
+///
+/// To use, import `package:masamune_animate/masamune_animate.dart`.
+///
+/// [mathru.net]: https://mathru.net
+/// [YouTube]: https://www.youtube.com/c/mathrunetchannel
+library masamune_animate;
+
+export 'package:flutter_animate/flutter_animate.dart';
