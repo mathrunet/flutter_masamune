3715759403e18fddee634931d033f0d92eff56c1
mathru <user@mathru.net>
2024-05-17 17:21:58 +0900

    feat: RuntimeFunctionsAdapter made testing easier.

--- packages/katana_functions/lib/adapter/runtime_functions_adapter.dart
+++ packages/katana_functions/lib/adapter/runtime_functions_adapter.dart
@@ -10,19 +10,31 @@ part of '/katana_functions.dart';
 class RuntimeFunctionsAdapter extends FunctionsAdapter {
   /// FunctionsAdapter] that only returns a normal system without communicating with the server side.
   ///
-  /// Use as a test stub.
+  /// You can test the processing of the intended function by specifying a stubbed function in [functions].
   ///
   /// サーバー側と通信せずに正常系を返すだけの[FunctionsAdapter]。
   ///
-  /// テスト用のスタブとして利用してください。
-  const RuntimeFunctionsAdapter();
+  /// [functions]にスタブ化した関数を指定することで意図した関数の処理をテストすることができます。
+  const RuntimeFunctionsAdapter({this.functions = const []});
+
+  /// Stubbed functions.
+  ///
+  /// スタブ化した関数。
+  final List<FunctionStub> functions;
 
   @override
   final String endpoint = "";
 
   @override
-  Future<TResponse> execute<TResponse>(FunctionsAction<TResponse> action) =>
-      action.execute((map) async => {});
+  Future<TResponse> execute<TResponse>(FunctionsAction<TResponse> action) {
+    final stub = functions.firstWhereOrNull(
+      (stub) => stub.functionName == action.action,
+    );
+    if (stub == null) {
+      return action.execute((input) async => {});
+    }
+    return action.execute((input) async => stub.process(input ?? {}));
+  }
 
   @override
   int get hashCode => runtimeType.hashCode;
