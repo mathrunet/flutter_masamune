47214767067de053635abdc475fe7acf9140f7e2
mathru <user@mathru.net>
2024-05-20 09:48:06 +0900

    feat: Initial commit.

---
+++ packages/katana_value/example/lib/main.dart
@@ -0,0 +1,26 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Package imports:
+import 'package:katana_value/katana_value.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return AuthAdapterScope(
+//       adapter: const RuntimeAuthAdapter(),
+//       child: MaterialApp(
+//         title: "Flutter Demo",
+//         theme: ThemeData(
+//           primarySwatch: Colors.blue,
+//         ),
+//       ),
+//     );
+//   }
+// }
---
+++ packages/katana_value/lib/katana_value.dart
@@ -0,0 +1,9 @@
+// Copyright (c) 2024 mathru. All rights reserved.
+
+/// This is a macro package for creating immutable data classes. It can be applied to widgets in addition to regular data.
+///
+/// To use, import `package:katana_value/katana_value.dart`.
+///
+/// [mathru.net]: https://mathru.net
+/// [YouTube]: https://www.youtube.com/c/mathrunetchannel
+library katana_value;
