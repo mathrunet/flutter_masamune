7c247af3350a2644dfd0d585a990752818a7351a
mathru <user@mathru.net>
2024-08-05 18:16:18 +0900

    fix: Firebase can now be initialized.

--- packages/katana_cli/lib/action/firebase/init.dart
+++ packages/katana_cli/lib/action/firebase/init.dart
@@ -573,6 +573,10 @@ class FirebaseFunctionsIndexCliCode extends CliCode {
   String import(String path, String baseName, String className) {
     return """
 import * as m from "@mathrunet/masamune";
+import * as admin from "firebase-admin";
+if (admin.apps.length === 0) {
+  admin.initializeApp();
+}
 
 """;
   }
