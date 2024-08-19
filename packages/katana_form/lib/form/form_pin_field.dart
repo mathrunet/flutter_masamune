4bf698e0c43fcbacb1b8ae14d0bf51688c0fe49b
Masaru Hirose (mathru@Kanimiso-P) <45095843+mathrunet@users.noreply.github.com>
2024-08-19 19:32:52 +0900

    fix: Fixed a bug that prevented Pin from being input on Android.

--- packages/katana_form/lib/form/form_pin_field.dart
+++ packages/katana_form/lib/form/form_pin_field.dart
@@ -124,7 +124,7 @@ class FormPinField<TValue> extends FormField<String> {
     this.form,
     this.controller,
     this.style,
-    super.enabled,
+    super.enabled = true,
     String? initialValue,
     this.focusNode,
     TextInputType keyboardType = TextInputType.phone,
@@ -230,7 +230,7 @@ class FormPinField<TValue> extends FormField<String> {
                 width: style?.width,
                 child: UnmanagedRestorationScope(
                   bucket: field.bucket,
-                  child: PinInputTextField(
+                  child: _PinInputTextField(
                     pinLength: maxLength,
                     controller: state._effectiveController,
                     focusNode: state._effectiveFocusNode,
@@ -251,6 +251,7 @@ class FormPinField<TValue> extends FormField<String> {
                     textInputAction: textInputAction,
                     textCapitalization: textCapitalization,
                     autocorrect: autocorrect,
+                    autoFocus: autofocus,
                     onChanged: onChangedHandler,
                     onSubmit: onSubmitted,
                     inputFormatters: inputFormatters,
@@ -369,11 +370,6 @@ class _FormPinFieldState<TValue> extends FormFieldState<String>
       _focusNode = FocusNode();
     }
     widget.form?.register(this);
-    WidgetsBinding.instance.scheduleFrameCallback((_) async {
-      if (mounted && widget.autofocus) {
-        FocusScope.of(context).autofocus(_effectiveFocusNode);
-      }
-    });
   }
 
   @override
@@ -448,4 +444,357 @@ class _FormPinFieldState<TValue> extends FormFieldState<String>
     super.build(context);
     return widget.builder(this);
   }
+}
+
+class _PinInputTextField extends StatefulWidget {
+  _PinInputTextField({
+    Key? key,
+    this.pinLength = kPinLength,
+    this.onSubmit,
+    required this.decoration,
+    this.inputFormatters,
+    this.keyboardType = TextInputType.phone,
+    this.controller,
+    this.focusNode,
+    this.autoFocus = false,
+    this.textInputAction = TextInputAction.done,
+    this.enabled = true,
+    this.onChanged,
+    textCapitalization,
+    this.autocorrect = false,
+    this.enableInteractiveSelection = false,
+    this.autofillHints,
+    Cursor? cursor,
+  })  : assert(pinLength > 0),
+        assert(decoration.hintText == null ||
+            decoration.hintText!.length == pinLength),
+        assert(decoration is! SupportGap ||
+            (decoration is SupportGap &&
+                    (decoration as SupportGap).getGapWidthList == null ||
+                (decoration as SupportGap).getGapWidthList!.length ==
+                    pinLength - 1)),
+        textCapitalization = textCapitalization ?? TextCapitalization.none,
+        cursor = cursor ?? Cursor.disabled(),
+        super(key: key);
+
+  @override
+  State createState() => _PinInputTextFieldState();
+
+  final int pinLength;
+
+  final ValueChanged<String>? onSubmit;
+
+  final PinDecoration decoration;
+
+  final List<TextInputFormatter>? inputFormatters;
+
+  final TextInputType keyboardType;
+
+  final TextEditingController? controller;
+
+  final bool autoFocus;
+
+  final FocusNode? focusNode;
+
+  final TextInputAction textInputAction;
+
+  final bool enabled;
+
+  final ValueChanged<String>? onChanged;
+
+  final TextCapitalization textCapitalization;
+
+  final bool autocorrect;
+
+  final bool enableInteractiveSelection;
+
+  final Iterable<String>? autofillHints;
+
+  final Cursor cursor;
+}
+
+class _PinInputTextFieldState extends State<_PinInputTextField>
+    with SingleTickerProviderStateMixin {
+  String _text = "";
+
+  TextEditingController? _controller;
+
+  late AnimationController _cursorBlinkOpacityController;
+  final ValueNotifier<bool> _cursorVisibilityNotifier =
+      ValueNotifier<bool>(true);
+  Timer? _cursorTimer;
+  bool _targetCursorVisibility = false;
+
+  Color _cursorColor = Colors.transparent;
+
+  TextEditingController? get _effectiveController =>
+      widget.controller ?? _controller;
+
+  void _pinChanged() {
+    setState(() => _updateText());
+  }
+
+  FocusNode? _focusNode;
+
+  FocusNode get _effectiveFocusNode =>
+      widget.focusNode ?? (_focusNode ??= FocusNode());
+
+  void _updateText() {
+    if (_effectiveController!.text.runes.length > widget.pinLength) {
+      _text = String.fromCharCodes(
+          _effectiveController!.text.runes.take(widget.pinLength));
+    } else {
+      _text = _effectiveController!.text;
+    }
+
+    widget.decoration.notifyChange(_text);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _cursorBlinkOpacityController =
+        AnimationController(vsync: this, duration: widget.cursor.fadeDuration);
+    _cursorBlinkOpacityController.addListener(_onCursorColorTick);
+    _cursorVisibilityNotifier.value = widget.cursor.enabled;
+    _effectiveFocusNode.addListener(_handleFocusChanged);
+    if (widget.controller == null) {
+      _controller = TextEditingController();
+    }
+    _effectiveController!.addListener(_pinChanged);
+
+    _updateText();
+    _startOrStopCursorTimerIfNeeded();
+  }
+
+  void _onCursorColorTick() {
+    if (widget.cursor.enabled) {
+      _cursorColor =
+          widget.cursor.color.withOpacity(_cursorBlinkOpacityController.value);
+      _cursorVisibilityNotifier.value = _cursorBlinkOpacityController.value > 0;
+      setState(() {});
+    }
+  }
+
+  @override
+  void dispose() {
+    _effectiveController!.removeListener(_pinChanged);
+    _cursorBlinkOpacityController.removeListener(_onCursorColorTick);
+    _stopCursorTimer();
+    _effectiveFocusNode.removeListener(_handleFocusChanged);
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(_PinInputTextField oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (widget.controller == null && oldWidget.controller != null) {
+      oldWidget.controller!.removeListener(_pinChanged);
+      _controller =
+          TextEditingController.fromValue(oldWidget.controller!.value);
+      _controller!.addListener(_pinChanged);
+    } else if (widget.controller != null && oldWidget.controller == null) {
+      _controller!.removeListener(_pinChanged);
+      _controller = null;
+      widget.controller!.addListener(_pinChanged);
+      if (_text != widget.controller!.text) {
+        _pinChanged();
+      }
+    } else if (widget.controller != oldWidget.controller) {
+      oldWidget.controller!.removeListener(_pinChanged);
+      widget.controller!.addListener(_pinChanged);
+    }
+
+    if (widget.focusNode != oldWidget.focusNode) {
+      (oldWidget.focusNode ?? _focusNode)?.removeListener(_handleFocusChanged);
+      (widget.focusNode ?? _focusNode)?.addListener(_handleFocusChanged);
+    }
+
+    if (oldWidget.pinLength > widget.pinLength &&
+        _text.runes.length > widget.pinLength) {
+      setState(() {
+        _text = _text.substring(0, widget.pinLength);
+        _effectiveController!.text = _text;
+        _effectiveController!.selection =
+            TextSelection.collapsed(offset: _text.runes.length);
+      });
+    }
+
+    widget.decoration.notifyChange(_text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFieldTapRegion(
+      child: _buildTextField(),
+    );
+  }
+
+  Widget _buildTextField() {
+    final pinPaint = _PinPaint(
+      text: _text,
+      pinLength: widget.pinLength,
+      decoration: widget.decoration,
+      themeData: Theme.of(context),
+      cursor: widget.cursor.copyWith(color: _cursorColor),
+      textDirection: Directionality.of(context),
+    );
+    return CustomPaint(
+      foregroundPainter: pinPaint,
+      child: TextField(
+        controller: _effectiveController,
+        style: TextStyle(
+          color: Colors.transparent,
+        ),
+        showCursor: false,
+        autofillHints: widget.autofillHints,
+        autocorrect: widget.autocorrect,
+        textAlign: TextAlign.center,
+        // enableInteractiveSelection: widget.enableInteractiveSelection,
+        maxLength: widget.pinLength,
+        onSubmitted: widget.onSubmit,
+        keyboardType: widget.keyboardType,
+        inputFormatters: widget.inputFormatters,
+        focusNode: _effectiveFocusNode,
+        autofocus: widget.autoFocus,
+        textInputAction: widget.textInputAction,
+        onChanged: widget.onChanged,
+        enabled: widget.enabled,
+        textCapitalization: widget.textCapitalization,
+        decoration: InputDecoration(
+          counterText: "",
+          errorText: widget.decoration.errorText,
+          errorStyle: widget.decoration.errorTextStyle,
+          enabled: false,
+          border: InputBorder.none,
+          focusedBorder: InputBorder.none,
+          enabledBorder: InputBorder.none,
+          errorBorder: InputBorder.none,
+          focusedErrorBorder: InputBorder.none,
+          disabledBorder: InputBorder.none,
+        ),
+      ),
+    );
+  }
+
+  void _cursorTick(Timer timer) {
+    _targetCursorVisibility = !_targetCursorVisibility;
+    final double targetOpacity = _targetCursorVisibility ? 1.0 : 0.0;
+    _cursorBlinkOpacityController.animateTo(targetOpacity,
+        curve: Curves.easeOut);
+  }
+
+  void _cursorWaitForStart(Timer timer) {
+    assert(widget.cursor.blinkHalfPeriod > widget.cursor.fadeDuration);
+    _cursorTimer?.cancel();
+    _cursorTimer = Timer.periodic(widget.cursor.blinkHalfPeriod, _cursorTick);
+  }
+
+  void _startCursorTimer() {
+    _targetCursorVisibility = true;
+    _cursorBlinkOpacityController.value = 1.0;
+    _cursorTimer =
+        Timer.periodic(widget.cursor.blinkWaitForStart, _cursorWaitForStart);
+  }
+
+  void _stopCursorTimer() {
+    _cursorTimer?.cancel();
+    _cursorTimer = null;
+    _targetCursorVisibility = false;
+    _cursorBlinkOpacityController.value = 0.0;
+    _cursorBlinkOpacityController.stop();
+    _cursorBlinkOpacityController.value = 0.0;
+  }
+
+  void _startOrStopCursorTimerIfNeeded() {
+    if (_cursorTimer == null && _hasFocus && _value.selection.isCollapsed) {
+      _startCursorTimer();
+    } else if (_cursorTimer != null &&
+        (!_hasFocus || !_value.selection.isCollapsed)) {
+      _stopCursorTimer();
+    }
+  }
+
+  bool get _hasFocus => _effectiveFocusNode.hasFocus;
+
+  TextEditingValue get _value => _effectiveController!.value;
+
+  void _handleFocusChanged() {
+    _startOrStopCursorTimerIfNeeded();
+  }
+}
+
+class _PinPaint extends CustomPainter {
+  final String text;
+  final int pinLength;
+  final PinEntryType type;
+  final PinDecoration decoration;
+  final ThemeData themeData;
+  Cursor? cursor;
+  TextDirection textDirection;
+
+  _PinPaint({
+    required this.text,
+    required this.pinLength,
+    required PinDecoration decoration,
+    this.type = PinEntryType.boxTight,
+    required this.themeData,
+    this.cursor,
+    this.textDirection = TextDirection.ltr,
+  }) : decoration = decoration.copyWith(
+          textStyle: decoration.textStyle ?? themeData.textTheme.headlineSmall,
+          errorTextStyle: decoration.errorTextStyle ??
+              themeData.textTheme.bodySmall
+                  ?.copyWith(color: themeData.colorScheme.error),
+          hintTextStyle: decoration.hintTextStyle ??
+              themeData.textTheme.headlineSmall
+                  ?.copyWith(color: themeData.hintColor),
+        );
+
+  @override
+  bool shouldRepaint(_PinPaint oldDelegate) => oldDelegate != this;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    decoration.drawPin(canvas, size, text, pinLength, cursor, textDirection);
+  }
+
+  _PinPaint copyWith({
+    String? text,
+    int? pinLength,
+    PinDecoration? decoration,
+    PinEntryType? type,
+    ThemeData? themeData,
+    Cursor? cursor,
+  }) =>
+      _PinPaint(
+        text: text ?? this.text,
+        pinLength: pinLength ?? this.pinLength,
+        decoration: decoration ?? this.decoration,
+        type: type ?? this.type,
+        themeData: themeData ?? this.themeData,
+        cursor: cursor ?? this.cursor,
+      );
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is _PinPaint &&
+          runtimeType == other.runtimeType &&
+          text == other.text &&
+          pinLength == other.pinLength &&
+          type == other.type &&
+          decoration == other.decoration &&
+          themeData == other.themeData &&
+          cursor == other.cursor;
+
+  @override
+  int get hashCode =>
+      text.hashCode ^
+      pinLength.hashCode ^
+      type.hashCode ^
+      decoration.hashCode ^
+      themeData.hashCode ^
+      cursor.hashCode;
 }
