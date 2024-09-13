1d801d2ce00b204038f534677d3918c8d3d81cf9
Masaru Hirose (mathru@Kanimiso-P) <45095843+mathrunet@users.noreply.github.com>
2024-09-13 14:15:28 +0900

    feat: Create a NotificationTargetQuery for the document.

--- packages/masamune_notification/lib/src/notification_target_query.dart
+++ packages/masamune_notification/lib/src/notification_target_query.dart
@@ -126,6 +126,75 @@ class TopicNotificationTargetQuery extends NotificationTargetQuery {
   }
 }
 
+/// Query to send PUSH notifications for all tokens in a specific document specific field.
+///
+/// 特定のドキュメント特定のフィールドにあるトークンすべてに対してPUSH通知を送信するためのクエリ。
+class DocumentNotificationTargetQuery extends NotificationTargetQuery {
+  /// Query to send PUSH notifications for all tokens in a specific document specific field.
+  ///
+  /// 特定のドキュメント特定のフィールドにあるトークンすべてに対してPUSH通知を送信するためのクエリ。
+  const DocumentNotificationTargetQuery({
+    required this.documentPath,
+    required this.tokenField,
+    this.conditions = const [],
+  });
+
+  /// The path of the collection from which to retrieve the document(s) to be notified.
+  ///
+  /// Must be in Firestore's canonical document path hierarchy.
+  ///
+  /// 通知の対象となるドキュメントを取得するコレクションのパス。
+  ///
+  /// Firestoreの正規のドキュメントのパス階層である必要があります。
+  final String documentPath;
+
+  /// Key to retrieve a list of eligible tokens.
+  ///
+  /// The target key must be defined in [ModelToken] or [List].
+  ///
+  /// 対象となるトークンのリストを取得するキー。
+  ///
+  /// 対象となるキーには[ModelToken]もしくは[List]で定義されている必要があります。
+  final ModelServerCommandField tokenField;
+
+  /// Specify the condition for the acquired data.
+  ///
+  /// All are And conditions.
+  ///
+  /// 取得したデータに対する条件を指定します。
+  ///
+  /// すべてAnd条件となります。
+  final List<ModelServerCommandCondition> conditions;
+
+  static const String _kDocumentPathKey = "targetDocumentPath";
+  static const String _kTokenFieldKey = "targetTokenField";
+  static const String _kConditionsKey = "targetConditions";
+
+  @override
+  DynamicMap toJson() {
+    assert(
+      !(documentPath.trimQuery().trimString("/").splitLength() <= 0 ||
+          documentPath.trimQuery().trimString("/").splitLength() % 2 != 0),
+      "The document path hierarchy must be an even number: $documentPath",
+    );
+    return <String, dynamic>{
+      _kDocumentPathKey: documentPath.trimQuery().trimString("/"),
+      _kTokenFieldKey: tokenField.toJson(),
+      _kConditionsKey: conditions.map((e) => e.toJson()).toList(),
+    };
+  }
+
+  @override
+  String toDatabaseId() {
+    return documentPath.replaceAll("/", "").limit(256);
+  }
+
+  @override
+  String toString() {
+    return "$runtimeType($documentPath)";
+  }
+}
+
 /// A query to send a PUSH notification for all tokens in a specific field, with conditions specified for a specific collection.
 ///
 /// 特定のコレクションに対して条件を指定し特定のフィールドにあるトークンすべてに対してPUSH通知を送信するためのクエリ。
@@ -183,8 +252,13 @@ class CollectionNotificationTargetQuery extends NotificationTargetQuery {
 
   @override
   DynamicMap toJson() {
+    assert(
+      !(collectionPath.trimQuery().trimString("/").splitLength() <= 0 ||
+          collectionPath.trimQuery().trimString("/").splitLength() % 2 != 1),
+      "The collection path hierarchy must be an odd number: $collectionPath",
+    );
     return <String, dynamic>{
-      _kCollectionPathKey: collectionPath,
+      _kCollectionPathKey: collectionPath.trimQuery().trimString("/"),
       _kTokenFieldKey: tokenField.toJson(),
       _kWheresKey: wheres.map((e) => e.toJson()).toList(),
       _kConditionsKey: conditions.map((e) => e.toJson()).toList(),
