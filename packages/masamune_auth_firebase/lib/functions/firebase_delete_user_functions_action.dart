d03341759111791dee13065a6fb02fd512e567fb
Masaru Hirose (mathru@Kanimiso-P) <45095843+mathrunet@users.noreply.github.com>
2024-12-16 20:04:48 +0900

    feat: Initial commit.

---
+++ packages/masamune_auth_firebase/lib/functions/firebase_delete_user_functions_action.dart
@@ -0,0 +1,58 @@
+part of '/masamune_auth_firebase.dart';
+
+/// [FunctionsAction] for deleting a user in FirebaseAuthentication.
+///
+/// FirebaseAuthenticationのユーザーを削除するための[FunctionsAction]。
+///
+/// {@macro functions_action}
+class FirebaseDeleteUserFunctionsAction
+    extends FunctionsAction<FirebaseDeleteUserFunctionsActionResponse> {
+  /// [FunctionsAction] for deleting a user in FirebaseAuthentication.
+  ///
+  /// FirebaseAuthenticationのユーザーを削除するための[FunctionsAction]。
+  ///
+  /// {@macro functions_action}
+  const FirebaseDeleteUserFunctionsAction({
+    required this.userId,
+  });
+
+  /// ID of the user to be deleted.
+  ///
+  /// 削除するユーザーID。
+  final String userId;
+
+  @override
+  String get action => "delete_user";
+
+  @override
+  DynamicMap? toMap() {
+    return {
+      "userId": userId,
+    };
+  }
+
+  @override
+  FirebaseDeleteUserFunctionsActionResponse toResponse(DynamicMap map) {
+    try {
+      if (map.isEmpty) {
+        throw Exception("Failed to get response from delete_user.");
+      }
+
+      return FirebaseDeleteUserFunctionsActionResponse();
+    } catch (e) {
+      debugPrint(e.toString());
+      rethrow;
+    }
+  }
+}
+
+/// Response to [FunctionsAction] for deleting a user in FirebaseAuthentication.
+///
+/// FirebaseAuthenticationのユーザーを削除するための[FunctionsAction]のレスポンス。
+class FirebaseDeleteUserFunctionsActionResponse
+    extends FunctionsActionResponse {
+  /// Response to [FunctionsAction] for deleting a user in FirebaseAuthentication.
+  ///
+  /// FirebaseAuthenticationのユーザーを削除するための[FunctionsAction]のレスポンス。
+  const FirebaseDeleteUserFunctionsActionResponse();
+}
---
+++ packages/masamune_auth_firebase/lib/masamune_auth_firebase.dart
@@ -0,0 +1,17 @@
+// Copyright (c) 2024 mathru. All rights reserved.
+
+/// Plug-in package for server-side integration of Firebase Authentication.
+///
+/// To use, import `package:masamune_auth_firebase/masamune_auth_firebase.dart`.
+///
+/// [mathru.net]: https://mathru.net
+/// [YouTube]: https://www.youtube.com/c/mathrunetchannel
+library masamune_auth_firebase;
+
+// Flutter imports:
+import 'package:flutter/widgets.dart';
+
+// Package imports:
+import 'package:masamune/masamune.dart';
+
+part 'functions/firebase_delete_user_functions_action.dart';
