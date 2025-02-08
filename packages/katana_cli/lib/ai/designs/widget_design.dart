f5b763c8677877461d62cf32a5ed6d1322216725
Masaru Hirose (mathru@Kanimiso-P) <45095843+mathrunet@users.noreply.github.com>
2025-02-08 22:23:31 +0900

    fix: Widget Design Documentation.

--- packages/katana_cli/lib/ai/designs/widget_design.dart
+++ packages/katana_cli/lib/ai/designs/widget_design.dart
@@ -19,7 +19,7 @@ class WidgetDesignMdcCliAiCode extends CliAiCode {
   String get directory => "designs";
 
   @override
-  String get description => "Masamuneフレームワークによる`Widget設計書`の作成";
+  String get description => "`Widget設計`の方法と`Widget設計書`の作成";
 
   @override
   String body(String baseName, String className) {
@@ -34,88 +34,10 @@ class WidgetDesignMdcCliAiCode extends CliAiCode {
         - 再利用する目的でなくただ複数の`Widget`をまとめるだけの場合
         - フォームなどの入力や選択を行う`Widget`
         - すでにFlutterに該当する`Widget`で代用可能なものが存在する場合
-        - Masamuneフレームワークで定義されている下記UniversalWidgetで代用可能な場合
-            - `UniversalScaffold`
-                - Flutter標準の`Scaffold`に下記機能が追加されたもの
-                    - SliverWidgetへの変換
-                    - PaddingやContainerを内蔵し`Scaffold`の外側に余白やボーダー、透明背景を設定可能
-                    - loadingFuturesを設定可能で非同期処理のローディングを表示可能
-            - `UniversalAppBar`
-                - Flutter標準の`AppBar`に下記機能が追加されたもの
-                    - `UniversalScaffold`と連携しSliverWidgetへの変換
-            - `UniversalListView`
-                - Flutter標準の`ListView`に下記機能が追加されたもの
-                    - `UniversalScaffold`と連携しSliverWidgetへの変換
-                    - onRefreshを設定可能でリストの更新を可能にする
-                    - onLoadNextを設定可能でリストの追加読み込みを可能にする
-                    - paddingやdecorationを設定可能でリストの外側に余白やボーダーを設定可能
-            - `UniversalColumn`
-                - Flutter標準の`Column`に下記機能が追加されたもの
-                    - `UniversalScaffold`と連携しSliverWidgetへの変換
-                    - paddingやdecorationを設定可能でリストの外側に余白やボーダーを設定可能
-            - `UniversalContainer`
-                - Flutter標準の`Container`に下記機能が追加されたもの
-                    - `UniversalScaffold`と連携しSliverWidgetへの変換
-        - Masamuneフレームワークで定義されている下記フォームWidgetで代用可能な場合
-            - `FormButton`
-                - Flutter標準の`Button`からMasamuneフレームワーク用にインターフェースを統一したもの
-            - `FormCheckbox`
-                - Flutter標準の`CheckBoxForm`からMasamuneフレームワーク用にインターフェースを統一したもの
-            - `FormChipsField`
-                - テキスト入力をChipとして表示し、文字のリストとして保存可能なフォーム。タグの入力等に利用。
-            - `FormDateField`
-                - 日付の入力が可能なフォーム。
-            - `FormDateTimeField`
-                - 日時の入力が可能なフォーム。
-            - `FormDateTimeRangeField`
-                - 日付の範囲を選択可能なフォーム。
-            - `FormDurationField`
-                - 時間の入力が可能なフォーム。
-            - `FormEditableToggleBuilder`
-                - 他のフォームをbuilderに含めることで編集可能な状態と編集不可能な状態を切り替えることが可能なフォーム。
-            - `FormEnumDropdownField`
-                - 列挙体の選択がドロップダウン形式で可能なフォーム。
-            - `FormEnumField`
-                - 列挙体の選択がボトムシートの選択肢から可能なフォーム。
-            - `FormFocusNodeBuilder`
-                - builderに他のフォームを含めることでこのWidget内で閉じたFocusNodeを利用可能にするフォーム。
-            - `FormFutureField`
-                - なにかしらの非同期処理を挟み、その結果を保存するためのフォーム。別ページを作成してそれを選択フォームとするような場合に利用。
-            - `FormLabel`
-                - フォームのUIを表示する際にラベルを表示するフォーム。
-            - `FormListBuilder`
-                - 複数のフォームをリスト表示し要素の追加削除が可能なフォームを作成するためのフォーム。
-            - `FormMapDropdownField`
-                - 連想配列の選択がドロップダウン形式で可能なフォーム。
-            - `FormMapField`
-                - 連想配列の選択がボトムシートの選択肢から可能なフォーム。
-            - `FormMapTagDropdownField`
-                - 連想配列の選択および、その追加、削除、ラベルの編集がドロップダウン形式で可能なフォーム。可変のタグを選択する場合に利用可能。
-            - `FormMedia`
-                - 画像や映像を保存するためのフォーム。
-            - `FormMonthField`
-                - 月の入力が可能なフォーム。
-            - `FormNumField`
-                - 数値の選択がボトムシートの選択肢から可能なフォーム。
-            - `FormPasswordBuilder`
-                - FormTextFieldと組み合わせることでパスワードの入力およびその表示非表示の切り替えが可能なフォーム。
-            - `FormPinField`
-                - ピン番号の入力が可能なフォーム。
-            - `FormRatingBar`
-                - 評価の選択が可能なフォーム。
-            - `FormStyleContainer`
-                - Masamuneフレームワークのフォームで利用するFormStyleを適用可能なContainer。
-            - `FormSwitch`
-                - Flutter標準の`Switch`からMasamuneフレームワーク用にインターフェースを統一したもの
-            - `FormTextEditingControllerBuilder`
-                - builderに他のフォームを含めることでこのWidget内で閉じたTextEditingControllerを利用可能にするフォーム。
-            - `FormTextField`
-                - Flutter標準の`TextField`からMasamuneフレームワーク用にインターフェースを統一したもの
-        - Masamuneフレームワークで定義されている下記ダイアログで代用可能な場合
-            - `Modal.alert`
-                - タイトル、メッセージ、１つのアクション（閉じる等）を設定可能なダイアログ。
-            - `Modal.confirm`
-                - タイトル、メッセージ、２つのアクション（決定、キャンセル等）を設定可能なダイアログ。
+        - [`UniversalUI`](mdc:.cursor/rules/docs/universal_ui_usage.mdc)で定義されている`Widget`で代用可能な場合
+        - [`KatanaUI`](mdc:.cursor/rules/docs/katana_ui_usage.mdc)で定義されている`Widget`で代用可能な場合
+        - [`Modal`](mdc:.cursor/rules/docs/modal_usage.mdc)で定義されている`Widget`で代用可能な場合
+        - [`Form`](mdc:.cursor/rules/docs/form_usage.mdc)で定義されている`Widget`で代用可能な場合
     - 各々の`Widget`に対して下記を定義
         - `WidgetName`
             - 末尾に`Widget`はつけない形でPascalCaseで定義
