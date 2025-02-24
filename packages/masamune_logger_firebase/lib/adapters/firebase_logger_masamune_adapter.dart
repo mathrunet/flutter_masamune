59245985953b9fe7b618de4238ff750c2ec4e5cc
Masaru Hirose (mathru@Kanimiso-P) <45095843+mathrunet@users.noreply.github.com>
2025-02-24 20:42:38 +0900

    fix: FirebaseOptions can now be specified in detail.

--- packages/masamune_logger_firebase/lib/adapters/firebase_logger_masamune_adapter.dart
+++ packages/masamune_logger_firebase/lib/adapters/firebase_logger_masamune_adapter.dart
@@ -40,12 +40,19 @@ class FirebaseLoggerMasamuneAdapter extends MasamuneAdapter {
   ///
   /// [analytics]や[crashlytics]、[performance]を渡すと[FirebaseAnalytics]や[FirebaseCrashlytics]、[FirebasePerformance]のインスタンスを指定可能です。
   const FirebaseLoggerMasamuneAdapter({
-    this.options,
+    FirebaseOptions? options,
+    this.iosOptions,
+    this.androidOptions,
+    this.webOptions,
+    this.linuxOptions,
+    this.windowsOptions,
+    this.macosOptions,
     FirebaseAnalytics? analytics,
     FirebaseCrashlytics? crashlytics,
     FirebasePerformance? performance,
     List<LoggerAdapter> loggerAdapters = const [],
   })  : _analytics = analytics,
+        _options = options,
         _crashlytics = crashlytics,
         _performance = performance;
 
@@ -76,13 +83,123 @@ class FirebaseLoggerMasamuneAdapter extends MasamuneAdapter {
 
   final FirebasePerformance? _performance;
 
-  /// [FirebaseOptions] set.
+  /// Options for initializing Firebase.
   ///
-  /// 設定されている[FirebaseOptions]。
-  final FirebaseOptions? options;
+  /// If platform-specific options are specified, they take precedence.
+  ///
+  /// Firebaseを初期化する際のオプション。
+  ///
+  /// プラットフォーム固有のオプションが指定されている場合はそちらが優先されます。
+  FirebaseOptions? get options {
+    if (UniversalPlatform.isIOS) {
+      return iosOptions ?? _options;
+    } else if (UniversalPlatform.isAndroid) {
+      return androidOptions ?? _options;
+    } else if (UniversalPlatform.isWeb) {
+      return webOptions ?? _options;
+    } else if (UniversalPlatform.isLinux) {
+      return linuxOptions ?? _options;
+    } else if (UniversalPlatform.isWindows) {
+      return windowsOptions ?? _options;
+    } else if (UniversalPlatform.isMacOS) {
+      return macosOptions ?? _options;
+    } else {
+      return _options;
+    }
+  }
+
+  /// Options for initializing Firebase.
+  ///
+  /// If options for other platforms are specified, these are ignored.
+  ///
+  /// Firebaseを初期化する際のオプション。
+  ///
+  /// 他のプラットフォーム用のオプションが指定されている場合はこちらは無視されます。
+  final FirebaseOptions? _options;
+
+  /// Options for initializing Firebase.
+  ///
+  /// Applies to IOS only.
+  ///
+  /// If [options] is specified, this takes precedence.
+  ///
+  /// Firebaseを初期化する際のオプション。
+  ///
+  /// IOSのみに適用されます。
+  ///
+  /// [options]が指定されている場合はこちらが優先されます。
+  final FirebaseOptions? iosOptions;
+
+  /// Options for initializing Firebase.
+  ///
+  /// Applies to Android only.
+  ///
+  /// If [options] is specified, this takes precedence.
+  ///
+  /// Firebaseを初期化する際のオプション。
+  ///
+  /// Androidのみに適用されます。
+  ///
+  /// [options]が指定されている場合はこちらが優先されます。
+  final FirebaseOptions? androidOptions;
+
+  /// Options for initializing Firebase.
+  ///
+  /// Applies to Web only.
+  ///
+  /// If [options] is specified, this takes precedence.
+  ///
+  /// Firebaseを初期化する際のオプション。
+  ///
+  /// Webのみに適用されます。
+  ///
+  /// [options]が指定されている場合はこちらが優先されます。
+  final FirebaseOptions? webOptions;
+
+  /// Options for initializing Firebase.
+  ///
+  /// Applies to Web only.
+  ///
+  /// If [options] is specified, this takes precedence.
+  ///
+  /// Firebaseを初期化する際のオプション。
+  ///
+  /// Webのみに適用されます。
+  ///
+  /// [options]が指定されている場合はこちらが優先されます。
+  final FirebaseOptions? windowsOptions;
+
+  /// Options for initializing Firebase.
+  ///
+  /// Applies to MacOS only.
+  ///
+  /// If [options] is specified, this takes precedence.
+  ///
+  /// Firebaseを初期化する際のオプション。
+  ///
+  /// MacOSのみに適用されます。
+  ///
+  /// [options]が指定されている場合はこちらが優先されます。
+  final FirebaseOptions? macosOptions;
+
+  /// Options for initializing Firebase.
+  ///
+  /// Applies to Linux only.
+  ///
+  /// If [options] is specified, this takes precedence.
+  ///
+  /// Firebaseを初期化する際のオプション。
+  ///
+  /// Linuxのみに適用されます。
+  ///
+  /// [options]が指定されている場合はこちらが優先されます。
+  final FirebaseOptions? linuxOptions;
 
   @override
   final bool runZonedGuarded = true;
+
+  @override
+  double get priority => 10.0;
 
   @override
   FutureOr<void> onPreRunApp() async {
