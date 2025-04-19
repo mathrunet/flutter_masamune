21c9175141950813e55a812d0b20d1e8d04f9304
Masaru Hirose (mathru@Kanimiso-P) <45095843+mathrunet@users.noreply.github.com>
2025-04-19 18:44:12 +0900

    fix: Size can be specified.

--- packages/katana_ui/lib/ui/square_avatar.dart
+++ packages/katana_ui/lib/ui/square_avatar.dart
@@ -20,12 +20,24 @@ class SquareAvatar extends StatelessWidget {
     this.radius,
     this.backgroundColor,
     this.backgroundImage,
+    this.width,
+    this.height,
   });
 
   /// Rounded corners.
   ///
   /// 角丸。
   final double? radius;
+
+  /// Width.
+  ///
+  /// 幅。
+  final double? width;
+
+  /// Height.
+  ///
+  /// 高さ。
+  final double? height;
 
   /// Background color.
   ///
@@ -39,19 +51,33 @@ class SquareAvatar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(radius ?? 0),
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          if (backgroundColor != null) ColoredBox(color: backgroundColor!),
-          if (backgroundImage != null)
-            Image(
-              image: backgroundImage!,
-              fit: BoxFit.cover,
-            ),
-        ],
+    return _buildSize(
+      context,
+      ClipRRect(
+        borderRadius: BorderRadius.circular(radius ?? 0),
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            if (backgroundColor != null) ColoredBox(color: backgroundColor!),
+            if (backgroundImage != null)
+              Image(
+                image: backgroundImage!,
+                fit: BoxFit.cover,
+              ),
+          ],
+        ),
       ),
     );
+  }
+
+  Widget _buildSize(BuildContext context, Widget child) {
+    if (width != null && height != null) {
+      return SizedBox(
+        width: width,
+        height: height,
+        child: child,
+      );
+    }
+    return child;
   }
 }
