c6793025dc5309781cf3858ee77c58f6a6fb906a
Masaru Hirose (mathru@Kanimiso-P) <support@mathru.net>
2025-07-14 22:27:05 +0900

    feat: Delete katana_listenable

--- packages/katana_listenables/example/android/app/src/main/kotlin/com/example/katana_listenables_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.katana_listenables_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- packages/katana_listenables_builder/lib/src/config.dart
+++
@@ -1,9 +0,0 @@
-part of "/katana_listenables_builder.dart";
-
-final _ignoreWords = <String>[
-  "dispose",
-  "addListener",
-  "removeListener",
-  "hasListeners",
-  "notifyListeners",
-];
