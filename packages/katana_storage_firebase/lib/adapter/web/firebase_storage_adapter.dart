8e31cf00eb202a3880589d2be1443a27747b493a
Masaru Hirose (mathru@Kanimiso-P) <support@mathru.net>
2025-07-20 14:30:22 +0900

    fix: Applicable to PlatformInfo.

--- packages/katana_storage_firebase/lib/adapter/web/firebase_storage_adapter.dart
+++ packages/katana_storage_firebase/lib/adapter/web/firebase_storage_adapter.dart
@@ -64,6 +64,8 @@ class FirebaseStorageAdapter extends StorageAdapter {
 
   String get _storageBucket => storage.bucket;
 
+  static const _platformInfo = PlatformInfo();
+
   /// Options for initializing Firebase.
   ///
   /// If platform-specific options are specified, they take precedence.
@@ -72,17 +74,17 @@ class FirebaseStorageAdapter extends StorageAdapter {
   ///
   /// プラットフォーム固有のオプションが指定されている場合はそちらが優先されます。
   FirebaseOptions? get options {
-    if (UniversalPlatform.isIOS) {
+    if (_platformInfo.isIOS) {
       return iosOptions ?? _options;
-    } else if (UniversalPlatform.isAndroid) {
+    } else if (_platformInfo.isAndroid) {
       return androidOptions ?? _options;
-    } else if (UniversalPlatform.isWeb) {
+    } else if (_platformInfo.isWeb) {
       return webOptions ?? _options;
-    } else if (UniversalPlatform.isLinux) {
+    } else if (_platformInfo.isLinux) {
       return linuxOptions ?? _options;
-    } else if (UniversalPlatform.isWindows) {
+    } else if (_platformInfo.isWindows) {
       return windowsOptions ?? _options;
-    } else if (UniversalPlatform.isMacOS) {
+    } else if (_platformInfo.isMacOS) {
       return macosOptions ?? _options;
     } else {
       return _options;
--- packages/katana_storage_firebase/lib/adapter/web/web.dart
+++ packages/katana_storage_firebase/lib/adapter/web/web.dart
@@ -7,6 +7,7 @@ import "package:flutter/foundation.dart";
 // Package imports:
 import "package:firebase_storage/firebase_storage.dart";
 import "package:katana_firebase/katana_firebase.dart";
+import "package:katana_platform_info/katana_platform_info.dart";
 import "package:katana_storage/katana_storage.dart";
 
 part "firebase_storage_adapter.dart";
