b714991489621aeddb46710e7ef631670d7809a5
Masaru Hirose (mathru@Kanimiso-P) <support@mathru.net>
2025-07-20 14:28:32 +0900

    chore: Remove unuse import.

--- packages/katana_storage_firebase/lib/adapter/web/web.dart
+++ packages/katana_storage_firebase/lib/adapter/web/web.dart
@@ -8,6 +8,5 @@ import "package:flutter/foundation.dart";
 import "package:firebase_storage/firebase_storage.dart";
 import "package:katana_firebase/katana_firebase.dart";
 import "package:katana_storage/katana_storage.dart";
-import "package:universal_platform/universal_platform.dart";
 
 part "firebase_storage_adapter.dart";
