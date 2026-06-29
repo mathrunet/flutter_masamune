aa9525c7424ba2636ec52aa0a94ec43f7f79426a
mathru <user@mathru.net>
2026-06-29 23:47:34 +0900

    fix(katana_cli): update comments to reflect 'functions' directory in Cloudflare init
    Correct comments in the Cloudflare initialization file to reference the new 'functions' directory instead of 'workers', ensuring clarity and consistency with recent structural changes.

--- packages/katana_cli/lib/action/cloudflare/init.dart
+++ packages/katana_cli/lib/action/cloudflare/init.dart
@@ -144,9 +144,9 @@ import * as m from "@mathrunet/masamune_cloudflare";
   @override
   String body(String path, String baseName, String className) {
     return """
-// Define [m.Workers.xxxx] for the functions to be added to Workers.
+// Define [m.Functions.xxxx] for the functions to be added to Workers.
 //
-// Workersに追加する機能を[m.Workers.xxxx]を定義してください。
+// Workersに追加する機能を[m.Functions.xxxx]を定義してください。
 export default m.deploy([
 ]);
 """;
